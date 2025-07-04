6bf8ec6404df555b8781496f0acfadd87f58fd26
Grecco <148456604+greccoparedon@users.noreply.github.com>
2025-07-04 11:10:42 +0000

    aula 2

---
+++ script.js
@@ -0,0 +1,7 @@
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
+botaoMostraPalavras.addEventListener('click',mostrapalavraschave);
+
+function mostrapalavraschave(){
+    alert('Buyu é mengão');
+}
